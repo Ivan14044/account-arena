871cb8a7951a27ce857de02749aa47bcb13de1f4
Ivan14044 <knysh_ivan@ukr.net>
2026-01-15 15:10:12 +0100

    SEO: Add service pages routes for SPA meta injection

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -217,6 +217,10 @@ Route::get('/account/{id}', [\App\Http\Controllers\Seo\SpaController::class, 'in
 Route::get('/articles/{id}', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->where('id', '\d+')->name('spa.article');
 Route::get('/articles', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.articles');
 Route::get('/categories/{id}', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->where('id', '\d+')->name('spa.category');
+Route::get('/become-supplier', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.become-supplier');
+Route::get('/conditions', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.conditions');
+Route::get('/payment-refund', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.payment-refund');
+Route::get('/contacts', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.contacts');
 Route::get('/', [\App\Http\Controllers\Seo\SpaController::class, 'index'])->name('spa.home');
 
 // Sitemap
